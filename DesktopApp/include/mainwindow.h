ea1a72ae2bd7350d15d32ca1949d37e4851c40c7
rflban <nabiev.alsn@yandex.ru>
2019-08-10 15:42:58 +0300

    add dir choosing slot

--- DesktopApp/include/mainwindow.h
+++ DesktopApp/include/mainwindow.h
@@ -16,8 +16,20 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+    enum Page
+    {
+        Laboratory,
+        Generating
+    };
+
 private:
     Ui::MainWindow *ui;
+
+    void connectAll();
+
+private slots:
+    void setCurrentPage(int idx);
+    void chooseDir();
 };
 
 #endif // MAINWINDOW_H_
--- DesktopApp/source/mainwindow.cc
+++ DesktopApp/source/mainwindow.cc
@@ -1,15 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connectAll();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::connectAll()
+{
+    connect(ui->labBtn, &QPushButton::clicked, this, [this] { setCurrentPage(Laboratory); });
+    connect(ui->genBtn, &QPushButton::clicked, this, [this] { setCurrentPage(Generating); });
+
+    connect(ui->dirChooseBtn, &QPushButton::clicked, this, &MainWindow::chooseDir);
+}
+
+void MainWindow::setCurrentPage(int idx)
+{
+    ui->pages->setCurrentIndex(idx);
+}
+
+void MainWindow::chooseDir()
+{
+    QString dirpath = QFileDialog::getExistingDirectory(this);
+
+    if (dirpath != "")
+    {
+        ui->dirPathEntry->setText(dirpath);
+    }
 }
 
